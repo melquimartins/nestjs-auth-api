3f8476d99e618ebb48a3e2b7ff05abad581fae18
Melqui Martins <115431086+melquimartins@users.noreply.github.com>
2026-08-13 13:19:43 -0300

    chore: configura estrutura inicial do projeto

---
+++ src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Post } from "@nestjs/common";
+import { AppService } from "./app.service";
+
+@Controller("users")
+export class AppController {
+    constructor(private readonly appService: AppService) {}
+
+    @Post("create")
+    createUser(): { message: string } {
+        return this.appService.createUser();
+    }
+}
